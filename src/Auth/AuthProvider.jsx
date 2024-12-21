f8e67c385bcd84f812211bfa339db8232a1662f4
abujaforhadi <abujaforhadi@gmail.com>
2024-12-22 01:25:41 +0600

    setup auth

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,38 +1,136 @@
-import React, { createContext, useState } from 'react';
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import Auth from '../Firebase/Firebase.config';
+import React, { useState, createContext, useEffect } from "react";
+import {
+ 
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Auth from "../Firebase/Firebase.config";
 
-
-
+const auth=Auth;
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [user, setUser] = useState(null)
-
-    const CreateUser = async (email, password) => {
-
+  const createNewUser = async (email, password) => {
+    setLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setUser(userCredential.user);
+      toast.success("Account created successfully!");
+    } catch (error) {
+      toast.error(`Error: ${error.message}`);
+    } finally {
+      setLoading(false);
     }
-     // implement google login
-    const loginWithGoogle=()=>{
-       
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(Auth, provider)
+  };
 
+  const ProfileUpdate = async (displayName, photoURL) => {
+    setLoading(true);
+    try {
+      await updateProfile(auth.currentUser, { displayName, photoURL });
+    } catch (error) {
+      console.error("Error updating profile:", error.message);
+      toast.error(`Error: ${error.message}`);
+    } finally {
+      setLoading(false);
     }
-    const userInfo = {
-        CreateUser,
-        loginWithGoogle
+  };
 
+ 
+
+  const login = async (email, password) => {
+    setLoading(true);
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setUser(userCredential.user);
+    } catch (error) {
+      // console.error("Login error:", error.message);
+      toast.error("Please input valid email or password");
+    } finally {
+      setLoading(false);
     }
+  };
 
+  const loginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      setUser(user);
+      toast.success("Google login successful!");
+    } catch (error) {
+      console.error("Error during Google login:", error.message);
+      toast.error(`Error: ${error.message}`);
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={userInfo}>
-            {children}
+  const logout = async () => {
+    setLoading(true);
+    try {
+      await signOut(auth);
+      setUser(null);
+      toast.success("Sign-out successful.");
+    } catch (error) {
+      console.error("Error during sign-out:", error.message);
+      toast.error(`Error: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        </AuthContext.Provider>
-    );
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    setUser,
+    createNewUser,
+    login,
+    logout,
+    loading,
+    ProfileUpdate,
+    loginWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light" 
+      />
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../Auth/AuthProvider';
 import { useLocation, useNavigate } from 'react-router';
 
