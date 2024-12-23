9b78ccc77f1731dd0fdee7714091a6c3e43f97a3
abujaforhadi <abujaforhadi@gmail.com>
2024-12-23 15:05:20 +0600

    add tutorails

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { Navigate, useLocation } from 'react-router';
+import Loading from '../Components/Loading';
+import { useContext } from 'react';
+import { AuthContext } from '../Auth/AuthProvider';
 
-const PrivateRouter = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRouter = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (user && user?.email) {
+    return children; 
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRouter;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Registration from "../Pages/Registration";
+import AddTutorials from "../Pages/AddTutorials";
+import PrivateRouter from "./PrivateRouter";
 
 export const router = createBrowserRouter([
     {
@@ -24,7 +26,13 @@ export const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <Registration />
-            }
+            },
+            {
+                path: '/add-tutorials',
+                element: <PrivateRouter> <AddTutorials /></PrivateRouter>
+               
+            },
+
         ]
     },
 ])
