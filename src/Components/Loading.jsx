200e2ac03047bfec09bb06e15c518297deedd88b
abujaforhadi <abujaforhadi@gmail.com>
2025-02-10 20:16:44 +0600

    loader

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import pic from '../assets/loading-rings.json'; 
+import React from "react";
+import pic from "../assets/loading-rings.json";
 import { useLottie } from "lottie-react";
 
 const Loading = () => {
-    const options = {
-        animationData: pic,
-        loop: true
-      };
-      const { View } = useLottie(options);
-    return (
-        <div className='w-1/5 mx-auto'>
-            {View}
-            
-        </div>
-    );
+  const options = {
+    animationData: pic,
+    loop: true,
+  };
+  const { View } = useLottie(options);
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="w-32 md:w-40 lg:w-40">{View}</div>
+    </div>
+  );
 };
 
 export default Loading;
